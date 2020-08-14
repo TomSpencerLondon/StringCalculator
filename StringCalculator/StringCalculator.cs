0db4f39e154c95ba843a27ba71403a7fa0827f0e
Tom <thomas.spencer@codurance.com>
2020-08-14 15:51:50 +0100

    Optimize imports

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Globalization;
 using System.Linq;
 using System.Text;
-using NUnit.Framework.Constraints;
 
 namespace StringCalculator
 {
